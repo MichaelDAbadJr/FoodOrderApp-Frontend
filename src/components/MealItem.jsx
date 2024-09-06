fb5f446d986eb065572fe6ce749b9ffe4ae82b72
Michael Abad <jun.abad@my.utsa.edu>
2024-09-05 23:23:22 -0500

    fixed url path for image

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -12,7 +12,7 @@ function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <img src={`{${VITE_BACKEND_URL}/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
